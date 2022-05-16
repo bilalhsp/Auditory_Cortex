d665b88503cefcd93ec0a5f1fec2265e9c7d429d
Akshita Ramya Kamsali <akamsali@purdue.edu>
2022-05-15 20:41:34 -0400

    mid unit

--- gradient_extraction/get_gradients.py
+++ gradient_extraction/get_gradients.py
@@ -12,22 +12,27 @@ class GetGradient():
         
         self.optimizer.zero_grad()
         
-        
         net_out = self.model(self.spect, decoder_input_ids=self.decoder_input_ids)
         # print(hook.output_f.shape)
-        beta_z = self.sig(torch.matmul(self.hook.output_f, self.betas))
-        # print(beta_z[0].shape)
-        loss = (1 - self.sig(beta_z[0])).mean()
+        # add 
+        # print("hf_shape:", self.hook.output_f.shape)
+        beta = torch.zeros(self.hook.output_f.shape[1], 1)
+        beta[beta.shape[0]//2, :] = 1
+        self.betas = beta
+        beta_z = torch.matmul(self.betas.T, self.hook.output_f)
+        # beta_z = torch.zeros_like(self.hook.output_f)
+        # beta_z[:,:,0] = self.hook.output_f[:, :, 0]
+        # self.beta_z = beta_z
+        loss = -beta_z[0].mean()
         # print("loss:", loss.item())
         loss.backward()
         self.optimizer.step()
         return loss.item()
 
-    def get_opt_input(self, spect, betas, layer, iterations=10):
+    def get_opt_input(self, spect, layer, iterations=10):
         self.spect = spect
         self.spect.requires_grad = True
 
-        self.betas = betas
         self.hook = Hook(self.model, layer, backward=True)
         self.decoder_input_ids = torch.tensor([[1, 1]]) * self.model.config.decoder_start_token_id
 
