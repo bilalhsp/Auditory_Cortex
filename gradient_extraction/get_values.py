3e5cab7c1cf2597e773079c889fa849cd19cfa02
Akshita Ramya Kamsali <akamsali@purdue.edu>
2022-06-10 17:23:23 -0400

    remove audio files

--- gradient_extraction/get_values.py
+++ gradient_extraction/get_values.py
@@ -3,18 +3,13 @@ from gradient_extraction.hook import Hook
 
 
 class GetValues:
-    def __init__(self, model, feature_extractor):
+    def __init__(self, model):
         self.model = model 
-        self.feature_extractor = feature_extractor
 
-    def get_layer_output(self, aud, layer):
+    def get_layer_output(self, spect, layer):
         self.hook = Hook(self.model, layer, backward=True)
         decoder_input_ids = torch.tensor([[1, 1]]) * self.model.config.decoder_start_token_id
-
-        self.spect = self.feature_extractor(aud, sampling_rate=16000, return_tensors="pt").input_features
         
-        net_out = self.model(self.spect, decoder_input_ids=decoder_input_ids)
-        # print(f"shape: {self.hook.output_f.shape}")
-        # chosen_feature = self.hook.output_f[0, unit, :]
+        net_out = self.model(spect, decoder_input_ids=decoder_input_ids)
 
-                
+        return self.hook.output_f
