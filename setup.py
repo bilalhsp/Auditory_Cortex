a870ccb56520544af7ff5cbe595ef9f4bc2994ca
Bilal Ahmed <bilalhsp@gmail.com>
2025-07-10 14:54:49 -0400

    refactor: deprecated unused scripts, added license

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Auditory_cortex",
-    version="0.0.1",
-    author="Akshita Ramya and Bilal Ahmed",
-    author_email="{akamsali, bahmed}@purdue.edu",
+    name="auditory_cortex",
+    version="0.1",
+    author="Bilal Ahmed",
+    author_email="ahmedb@purdue.edu",
     description="Auditory Cortex Modeling with ECoG data in PyTorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
